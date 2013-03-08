919f9a69c5b4657713886babd08f7327f6e8f2fb
James Hopper <esendexdev+jimgolfgti@gmail.com>
2013-03-08 15:45:40 +0000

    Add License to doc comments

--- src/Esendex/Http/HttpClient.php
+++ src/Esendex/Http/HttpClient.php
@@ -1,4 +1,37 @@
 <?php
+/**
+ * Copyright (c) 2013, Esendex Ltd.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Esendex nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   Http
+ * @package    Esendex
+ * @author     Esendex Support <support@esendex.com>
+ * @copyright  2013 Esendex Ltd.
+ * @license    http://opensource.org/licenses/BSD-3-Clause  BSD 3-Clause
+ * @link       https://github.com/esendex/esendex-php-sdk
+ */
 namespace Esendex\Http;
 use Esendex\Authentication\IAuthentication;
 
@@ -70,7 +103,6 @@ class HttpClient implements IHttp
         $curlHandle = \curl_init();
 
         \curl_setopt($curlHandle, CURLOPT_URL, $url);
-        \curl_setopt($curlHandle, CURLOPT_HTTPHEADER, $httpHeaders);
         \curl_setopt($curlHandle, CURLOPT_FAILONERROR, 1);
         \curl_setopt($curlHandle, CURLOPT_FOLLOWLOCATION, 1); // Allow redirects.
         \curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
@@ -81,8 +113,11 @@ class HttpClient implements IHttp
         \curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, $method);
         if ($method == 'PUT' || $method == 'POST') {
             \curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
-            $httpHeaders[] = 'Content-Length: ' . strlen($data);
+			if (strlen($data) == 0) {
+				$httpHeaders[] = 'Content-Length: 0';
+			}
         }
+        \curl_setopt($curlHandle, CURLOPT_HTTPHEADER, $httpHeaders);
 
         $result = \curl_exec($curlHandle);
         $curlInfo = \curl_getinfo($curlHandle);
