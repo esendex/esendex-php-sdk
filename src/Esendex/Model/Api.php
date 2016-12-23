7dec264dfaffc1ad45444cf983c4d8a2c39cd333
Ben Smith and James Turner <development+BnSmth+jamesturner@esendex.com>
2016-12-23 11:43:07 +0000

    SVY-273: Update version to 2.0.0

--- src/Esendex/Model/Api.php
+++ src/Esendex/Model/Api.php
@@ -38,8 +38,8 @@ class Api
 {
     const NS = "http://api.esendex.com/ns/";
 
-    private static $major = 1;
-    private static $minor = 3;
+    private static $major = 2;
+    private static $minor = 0;
     private static $patch = 0;
 
     public static function getVersion()
